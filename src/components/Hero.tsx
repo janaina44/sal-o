5a65328dfb0f148905f24c3d111da1104a4a18f9
Janaina Da Silva <janinha4488@gmail.com>
2025-09-10 12:42:35 -0300

    pequenas alterações

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,18 +3,14 @@ import heroDesktop from "../assets/Hero.jpg";
 
 function Hero() {
   return (
-    <section
-      className="hero"
-      id="home"
-      style={{ backgroundImage: `url(${heroDesktop})` }} // imagem como background
-    >
+    <section className="hero" id="home">
+      <img src={heroDesktop} alt="Hero" className="hero__img" />
+
       <div className="hero__overlay">
         <div className="container hero__content">
-          <h1>Realce sua Beleza Natural ✨</h1>
-          <p>Transforme seu visual.</p>
-          <a href="#agendamento" className="btn">
-            Agende seu horário
-          </a>
+          <h1>Transforme seu visual✨</h1>
+          
+          <a href="#agendamento" className="btn">Agende seu horário</a>
         </div>
       </div>
     </section>
